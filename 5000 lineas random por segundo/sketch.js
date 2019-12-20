6ba77930322347b46b562e8cd0f04e5f3e3e43d3
nkodragas <54560225+nkodragas@users.noreply.github.com>
2019-12-20 11:15:17 -0300

    1000

---
+++ 5000 lineas random por segundo/sketch.js
@@ -0,0 +1,44 @@
+
+let n = 1;
+let i = 0;
+let edge = 400;
+
+let m=50;
+function setup() {
+
+  let str = random(0.5, 1);
+  createCanvas(800, 500);
+  frameRate(5)
+  strokeWeight(str);
+  stroke(0,50);
+
+}
+
+function draw() {
+lineas()
+  
+  }
+
+
+function lineas() {
+  background(220);
+  
+  for(let i = 0; i < 10000; i++){
+ 
+    push();
+    
+    let posX = random(m, width - m);
+    let posY = random(m, height - m);
+    
+    
+  
+  
+  translate(posX, posY);
+    
+  rotate(random(TWO_PI));
+    line(-20,0,20,0);
+    
+    pop();
+      }
+}
+  
